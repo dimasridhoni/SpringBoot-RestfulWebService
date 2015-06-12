ae5e320ef0ae3e6398a814bdb36df54cca51b4dc
DimasRidhoni <dimasridhoni@gmail.com>
2015-06-12 16:22:13 +0800

    Commit Dimas Ridhoni

---
+++ src/main/java/demo/BelajarwsApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelajarwsApplication.class, args);
+    }
+}
---
+++ src/main/java/demo/InstallDao.java
@@ -0,0 +1,8 @@
+package demo;
+
+import java.util.List;
+
+public interface InstallDao {
+	public List<Install> findAll();
+	
+}
